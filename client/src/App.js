fee49274c8ee1ef6297424eb2d1a643564d95986
Vincent Trivett <trivett@gmail.com>
2018-02-18 17:22:49 -0500

    installed semantic ui

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    window.fetch('api/cat_names')
+      .then(response => response.json())
+      .then(json => console.log(json))
+      .catch(error => console.log(error))
+  }
+
+
   render() {
     return (
       <div className="App">
